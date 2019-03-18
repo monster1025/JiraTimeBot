890fa1abd552302500e635791f6ad9366f00bb6d
Danil Kislov <Kislov@aeroclub.ru>
2019-03-18 22:54:40 +0300

    ok

---
+++ release.cmd
@@ -0,0 +1,16 @@
+@echo off
+for /f "tokens=1 delims=." %%G IN (semver.txt) DO set MAJOR=%%G 
+for /f "tokens=2 delims=." %%G IN (semver.txt) DO set MINOR=%%G 
+for /f "tokens=3 delims=." %%G IN (semver.txt) DO set PATCH=%%G
+set VERSION_CURRENT=%MAJOR:~0,1%.%MINOR:~0,1%.%PATCH%
+echo Current: %VERSION_CURRENT%
+
+SET /a PATCH1 = %PATCH%+1
+set VERSION=%MAJOR:~0,1%.%MINOR:~0,1%.%PATCH1%
+echo %VERSION%>semver.txt
+echo Updating to %VERSION%
+
+git add .
+git tag -a v%VERSION%
+git push
+pause
