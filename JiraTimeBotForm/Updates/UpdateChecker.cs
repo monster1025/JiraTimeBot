abda6857ab6dd6497b2f12b03e09163b3af8951f
Danil Kislov <Kislov@aeroclub.ru>
2019-03-18 21:03:31 +0300

    travis test

---
+++ JiraTimeBotForm/Updates/UpdateChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JiraTimeBotForm.Updates
+{
+    public class UpdateChecker
+    {
+        public bool NewVersionAvilable()
+        {
+            var current = Application.ProductVersion;
+            return false;
+        }
+    }
+}
