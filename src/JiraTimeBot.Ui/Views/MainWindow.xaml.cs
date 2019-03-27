c8465c504d855edac2d72bb32a84998773b9fd20
Anton Volkov <whirlwind432@gmail.com>
2019-03-27 16:33:05 +0300

    Minimize to tray andd icon

--- src/JiraTimeBot.Ui/Views/MainWindow.xaml.cs
+++ src/JiraTimeBot.Ui/Views/MainWindow.xaml.cs
@@ -1,15 +1,38 @@
-﻿namespace JiraTimeBot.Ui
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace JiraTimeBot.Ui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-
-
         public MainWindow()
         {
             InitializeComponent();
+
+            NotifyIcon trayIcon = new System.Windows.Forms.NotifyIcon();
+            trayIcon.Icon = new Icon("app.ico");
+            trayIcon.Visible = true;
+            trayIcon.DoubleClick += (sender, args) =>
+            {
+                Show();
+                ShowInTaskbar = true;
+                WindowState = WindowState.Normal;
+            };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                Hide();
+            }
+            base.OnStateChanged(e);
         }
     }
 }
